7ef0a89a88801c762cfc31b4f36a8ef8a30dbefa
Kushal <kush100.kj@gmail.com>
2023-03-24 20:56:50 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, {Marker, Callout, Circle} from 'react-native-maps';
+import * as Location from "expo-location"
+import * as React from 'react'
+import { Dimensions } from 'react-native';
 
 export default function App() {
+  const [Pin,setPin] = React.useState({
+    latitude: 18.521761780493907,
+    longitude: 73.85594520717859, 
+  })
+  
+  React.useEffect(() => {
+    (async () => {
+      let {status} = await Location.requestForegroundPermissionsAsync();
+      if (status !== "Granted") {
+        setErrorMsg("Permission Denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      // setLocation(location);
+
+      setPin({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      });
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      {/* <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" /> */}
+      <MapView style={styles.map} 
+      initialRegion={{
+        latitude: 18.521761780493907,
+        longitude: 73.85594520717859,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+    }}
+    showsUserLocation={true}
+    onUserLocationChange = {(e) => {
+      console.log("onUserLocationChange: ",e.nativeEvent.coordinate);
+      setPin({
+        latitude: e.nativeEvent.coordinate.latitude,
+        longitude: e.nativeEvent.coordinate.longitude
+      });
+    }}
+      >
+        <Marker coordinate={{latitude: 18.521761780493907, longitude: 73.85594520717859}}
+        title="Entered coordinates"
+        description="Pin points to the Requested Location coordinates"
+        pinColor="gold"
+        draggable={true}
+        onDragStart={(e) => {
+          console.log("drag start", e.nativeEvent.coordinate);
+        }}
+        onDragEnd={(e) => {
+          console.log("drag end", e.nativeEvent.coordinate);
+
+          setPin({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude
+          });
+       }}
+        >
+          <Callout>
+            <Text>This is Callout</Text>
+          </Callout>
+        </Marker>
+        <Circle center={Pin}
+        radius={1000}/>
+      </MapView>
+    </View> 
   );
 }
 
@@ -16,5 +82,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
   },
 });
